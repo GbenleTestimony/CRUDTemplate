1581f436dbf10f166bd0788bbd2fa7e7dd29838a
GbenleTestimony <testimonygbenle@gmail.com>
2026-01-15 00:56:06 -0800

    about to delete package.json

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,78 +1,78 @@
 import User from "../models/user.model.js";
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
 import { accessToken, refreshToken } from "../utils/generateToken.js";
 
-
 export const loginUser = async (req, res) => {
-    const {name, password} = req.body;
+  const { name, password } = req.body;
 
-    try{
-        if(!name || !password){
-            return res.status(400).json({message: 'Please enter all fields'})
-        }
-        const user = await User.findOne({ name });
-
-        if(!user){
-            return res.status(404).json({ message: 'User not found'})
-        } 
-        const isMatch = await bcrypt.compare(req.body.password, user.password)
-        if(!isMatch){
-            return res.status(400).json({message: 'Invalid password'})
-        }
-        const varAccess = accessToken(user.id)
-        return res.json({message:'Login successful'}, {accessToken: varAccess})
-        
-    }catch (err){
-        console.error('login error', err);
-        return res.status(500).json({message:'Server Error'});
+  try {
+    if (!name || !password) {
+      return res.status(400).json({ message: "Please enter all fields" });
     }
-}
-export const updateUser = async(req, res) => {
-    // Implementation for updating user details
-    const{name, newpassword}= req.body;
+    const user = await User.findOne({ name });
 
-    try{
-        userProfile = await User.findOne({name})
-
-        const newHash = await bcrypt.hash(newpassword, 10);
-
-        const newUser = await User.updateOne({name}, {password: newHash});
-
-        return res.status(201).json({message:'Password updated successfully'})
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-    catch(error){
-        console.error('Error Dey', error);
-        return res.status(500).json({message:'Something is wrong'});
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid password" });
     }
-}
-export const createUser = async(req, res)=>{
-    const{name, password}= req.body;
-
-    try{
-        if(!name || !password){
-            return res.status(400).json({message:'All fields are required'})
-        }
-        const exists = await User.findOne({name})
-        if(exists){
-            return res.status(409).json({message:'Name already taken'})
-        }
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        const newUser = await User.create({name,
-            password: hashedPassword,
-        })
-        const userResponse={
-            _id: newUser._id,
-            name: newUser.name,
-        }
-        return res.status(201).json({
-            message:'user created successfully',
-            user: userResponse
-        })
-    }
-    catch(error){
-        console.error('Error Dey', error);
-        return res.status(500).json({message:'Something is wrong'});
-    }
+    const varAccess = accessToken(user.id);
+    return res.json(
+      { message: "Login successful" },
+      { accessToken: varAccess }
+    );
+  } catch (err) {
+    console.error("login error", err);
+    return res.status(500).json({ message: "Server Error" });
+  }
 };
+export const updateUser = async (req, res) => {
+  // Implementation for updating user details
+  const { name, newPassword } = req.body;
 
+  try {
+    const possible = await User.findOne({name})
+    if(possible){
+      console.log('on it')
+      const newHash = await bcrypt.hash(newPassword, 10);
+      const updatedUser = await User.updateOne({name: name}, {password: newHash})
+        const updateResponse ={
+        name: updatedUser.id,
+        _id: updatedUser._id
+      };
+      return res.status(201).json({ message: "Password updated", response: updateResponse});
+    }    
+  } catch (error) {
+    console.error("Error Dey", error);
+    return res.status(500).json({ message: "Something is wrong" });
+  }
+};
+export const createUser = async (req, res) => {
+  const {name, password} = req.body;
+
+  try {
+    if (!name || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const exists = await User.findOne({ name });
+    if (exists) {
+      return res.status(409).json({ message: "Name already taken" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({ name, password: hashedPassword });
+    const userResponse = {
+      _id: newUser._id,
+      name: newUser.name,
+    };
+    return res.status(201).json({
+      message: "user created successfully",
+      user: userResponse,
+    });
+  } catch (error) {
+    console.error("Error Dey", error);
+    return res.status(500).json({ message: "Something is wrong" });
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import dotenv from "dotenv" // gets data from the environment
-dotenv.config({silent:true}) // what the hell is this for?
+dotenv.config({silent:true}, {path: '/custom/path/.env'})
+import path from 'path'; // what the hell is this for?
 import express from "express"; // for routing, handling requests
 import cors from "cors";
 import connectDB from "./config/db.js";
 import loginUser from "./routes/login.routes.js";
 import createUser from "./routes/signup.routes.js";
-import updateuser from "./routes/update.routes.js";
+import updateUser from "./routes/update.routes.js";
 // Creating the server
 const app = express();
 
@@ -23,15 +24,15 @@ app.use(express.json());// keeps all responses in json
 
 // Routes
 app.get('/', (req, res) => {
-    res.json({ message: 'API is running hale and hearty' });// default route and response in the browser
+    res.json({ message: 'Alive and active ' });// default route and response in the browser
 })
 app.use('/login', loginUser); // ./login here shows why the route in the router is only '/'
 app.use('/create', createUser);
-app.use('/update', updateuser);
+app.use('/update', updateUser);
 
 const PORT= process.env.PORT || 3000;
 
 //Starting server
 app.listen(PORT, ()=> {
-    console.log(`server is running fine on port ${PORT}`)
+    console.log(`server is running on port ${PORT}`)
 })// This keeps the backend alive on the specified port
