9b69d9ea772e42cc9ccee63cae012219e73ac64f
GbenleTestimony <testimonygbenle@gmail.com>
2026-09-05 15:31:24 +0100

    file case troubles 3

--- src/Routes/admin_routes.js
+++ src/Routes/admin_routes.js
@@ -6,8 +6,8 @@ import { tokenVerifier } from "../middlewares/token_verification.js";
 import { updateUsers } from "../admin_controllers/update_users.js";
 import { updateProducts } from "../admin_controllers/update_products.js";
 
-adminRouter.use('/users', tokenverifier, updateUsers);
-adminRouter.use('/products', tokenverifier ,updateProducts);
+adminRouter.use('/users', tokenVerifier, updateUsers);
+adminRouter.use('/products', tokenVerifier ,updateProducts);
 
 export default adminRouter;
 
