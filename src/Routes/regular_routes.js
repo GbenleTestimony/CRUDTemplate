d3483364261d3b739733516b55310e325eafba4b
GbenleTestimony <testimonygbenle@gmail.com>
2026-09-05 08:31:59 +0100

    last commit but now it is in other route files

--- src/Routes/regular_routes.js
+++ src/Routes/regular_routes.js
@@ -1,8 +1,8 @@
 import express from 'express';
 const regularRouter = express.Router();
 
-import { login } from '../Regular_controllers/login';
-import { signup } from '../Regular_controllers/signup';
+import { login } from '../Regular_controllers/login.js';
+import { signup } from '../Regular_controllers/signup.js';
 
 regularRouter.use('/login', login);
 regularRouter.use('/create', signup);
