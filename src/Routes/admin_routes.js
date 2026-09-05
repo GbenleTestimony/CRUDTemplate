71998e07bf35a995842a76d3ebe0b43be52ce761
GbenleTestimony <testimonygbenle@gmail.com>
2026-09-05 08:25:04 +0100

    esmodule standard enacted in middleware import

--- src/Routes/admin_routes.js
+++ src/Routes/admin_routes.js
@@ -1,9 +1,9 @@
 import express from "express";
 const adminRouter = express.Router();
 
-import { tokenverifier } from "../Middlewares/token_verification";
-import { updateUsers } from "../Admin_controllers/update_users";
-import { updateProducts } from "../Admin_controllers/update_products";
+import { tokenverifier } from "../Middlewares/token_verification.js";
+import { updateUsers } from "../Admin_controllers/update_users.js";
+import { updateProducts } from "../Admin_controllers/update_products.js";
 
 adminRouter.use('/users', tokenverifier, updateUsers);
 adminRouter.use('/products', tokenverifier ,updateProducts);
