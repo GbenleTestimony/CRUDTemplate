5d4ac63cc671902ac6dbea666b5cff1aff88aca4
GbenleTestimony <testimonygbenle@gmail.com>
2026-09-05 15:58:48 +0100

    tokenverifier to tokenVerifier

--- src/routes/regular_secured_routes.js
+++ src/routes/regular_secured_routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 const securedRouter = express.Router();
 
-import { tokenverifier } from "../middlewares/token_verification.js";
+import { tokenVerifier } from "../middlewares/token_verification.js";
 import { updateUser } from "../secured_controllers/update_user.js";
 
 securedRouter.use('/user', tokenverifier, updateUser);
