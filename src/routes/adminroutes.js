6a1d476d940a604ceb990e731b2ae85ab69dbe9d
GbenleTestimony <testimonygbenle@gmail.com>
2026-09-05 11:20:58 +0100

    wrong revert

--- src/routes/adminroutes.js
+++ src/routes/adminroutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 const adminRouter = express.Router();
 
-import { tokenverifier } from "../Middlewares/tokenverification.js";
-import { updateUsers } from "../Admincontrollers/updateusers.js";
-import { updateProducts } from "../Admincontrollers/updateproducts.js";
+
+import { tokenverifier } from "../Middlewares/token_verification";
+import { updateUsers } from "../Admin_controllers/update_users";
+import { updateProducts } from "../Admin_controllers/update_products";
 
 adminRouter.use('/users', tokenverifier, updateUsers);
 adminRouter.use('/products', tokenverifier ,updateProducts);
