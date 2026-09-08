caf21a1996f91d7aa871b715e0f1490c55c6904e
GbenleTestimony <testimonygbenle@gmail.com>
2026-09-08 23:00:17 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,8 @@ app.use(cors({
 
 
 // Home Route
-app.get('/', (res) => {
+app.get('/', (req, res) => {
+const { name } = req.body
     res.status(200).json({ message: {
         message: "server hit"
     } });
